2d797b6245d3677e8af02096e4376518865ddf44
Mohamed Azarkan <mohamed.azarkan@randstadgroup.be>
2026-01-09 14:15:27 +0100

    Add debug to provider create method

--- lib/puppet/provider/wls_jms_batch/simple.rb
+++ lib/puppet/provider/wls_jms_batch/simple.rb
@@ -10,6 +10,13 @@ Puppet::Type.type(:wls_jms_batch).provide(:simple) do
   # Override exists? to always return false, forcing creation
   # Batch resources don't have persistent state - they're just a wrapper for bulk operations
   def exists?
+    Puppet.debug "wls_jms_batch exists? called - returning false to force creation"
     false
+  end
+  
+  # Override create to ensure it's called
+  def create
+    Puppet.debug "wls_jms_batch create method called"
+    super
   end
 end
